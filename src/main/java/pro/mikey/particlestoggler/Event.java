ab0b95e3f0af9141371524ef72cf09be4fc923a5
Michael Hillcox <michaelhillcox@gmail.com>
2020-05-14 14:22:04 +0100

    Updated to 1.10, updated all code, cleaned up useless stuff :D

---
+++ src/main/java/pro/mikey/particlestoggler/Event.java
@@ -0,0 +1,38 @@
+package pro.mikey.particlestoggler;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod.EventBusSubscriber(Side.CLIENT)
+public class Event {
+
+    @SubscribeEvent
+    public static void handleKeyInput(final InputEvent.KeyInputEvent event)
+    {
+        Minecraft mc = Minecraft.getMinecraft();
+
+        if( ParticleToggler.toggle.isKeyDown() )
+        {
+            final int particlesSettings = mc.gameSettings.particleSetting;
+            if( particlesSettings == 0 )
+            {
+                mc.gameSettings.particleSetting = 1;
+                mc.thePlayer.addChatMessage(new TextComponentTranslation("particlestoggler.decreased"));
+            }
+            if( particlesSettings == 1 )
+            {
+                mc.gameSettings.particleSetting = 2;
+                mc.thePlayer.addChatMessage(new TextComponentTranslation("particlestoggler.minimal"));
+            }
+            if( particlesSettings == 2 )
+            {
+                mc.gameSettings.particleSetting = 0;
+                mc.thePlayer.addChatMessage(new TextComponentTranslation("particlestoggler.all"));
+            }
+        }
+    }
+}
