a2bb579c1aa71c6e6cdad38dd2a50217899a2c10
Michael Hillcox <michaelhillcox@gmail.com>
2020-05-14 15:16:48 +0100

    Updated to 1.12

--- src/main/java/pro/mikey/particlestoggler/ParticleToggler.java
+++ src/main/java/pro/mikey/particlestoggler/ParticleToggler.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.lwjgl.input.Keyboard;
 
-@Mod(modid = "particlestoggler", version = "1.11.2-1.2.0", name = "ParticleToggler", clientSideOnly = true)
+@Mod(modid = "particlestoggler", version = "1.12.2-1.3.0", name = "ParticleToggler", clientSideOnly = true)
 public class ParticleToggler
 {
     static KeyBinding toggle = new KeyBinding("Toggle Particles", Keyboard.KEY_P, "particlestoggler.name");
